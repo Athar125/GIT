b255bb7ddde43d5345660354d5d7c2642f9d99da
atharatta2@gmail.com <atharatta2@gmail.com>
2023-08-27 17:05:21 +0500

    changes

--- css.ts
+++ css.ts
@@ -1,3 +1,14 @@
 // MAP => return same lenght array
 // FILTER => return same lenght or less
 // FOREACH => return nothing
+
+let numbers:number[] = [2, 3, 4, 5, 6, 7, 100, 100033,]
+// even odd odd
+let updateArray = numbers.map((number)=> {
+    if (number % 2=== 0){
+        return "even";
+    } else {
+        return "odd"
+    }
+})
+console.log(updateArray);
