19faea3aa6218805b35f0e4f032c97ca5a75e4d4
atharatta2@gmail.com <atharatta2@gmail.com>
2023-08-28 22:53:38 +0500

    changes

--- assign7.ts
+++ assign7.ts
@@ -77,10 +77,11 @@ console.log(arrayOfNum);
 
 // Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach
 //  method to log each name with an exclamation mark at the end, e.g., "Alice!"
-
+/*
 let arrayOfNames : string[] = ["Alice", "Bob", "Charlie", "David", "Emily"] ;
 
 arrayOfNames.forEach(names => {
     console.log([`${names}!`]);
 
 })
+*/
