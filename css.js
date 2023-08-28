256f9f6462cd80de04811b8b65cd5f0b63ad4e18
atharatta2@gmail.com <atharatta2@gmail.com>
2023-08-28 22:52:29 +0500

    new Folder

--- css.js
+++ css.js
@@ -2,14 +2,16 @@
 // MAP => return same lenght array
 // FILTER => return same lenght or less
 // FOREACH => return nothing
-let numbers = [2, 3, 4, 5, 6, 7, 100, 100033,];
+/*
+let numbers:number[] = [2, 3, 4, 5, 6, 7, 100, 100033,]
 // even odd odd
-let updateArray = numbers.map((number) => {
-    if (number % 2 === 0) {
+let updateArray = numbers.map((number)=> {
+    if (number % 2=== 0){
         return "even";
+    } else {
+        return "odd"
     }
-    else {
-        return "odd";
-    }
-});
+})
 console.log(updateArray);
+*/
+// let updateArray = numbers.map(()=>{});
--- css.ts
+++ css.ts
@@ -1,7 +1,7 @@
 // MAP => return same lenght array
 // FILTER => return same lenght or less
 // FOREACH => return nothing
-
+/*
 let numbers:number[] = [2, 3, 4, 5, 6, 7, 100, 100033,]
 // even odd odd
 let updateArray = numbers.map((number)=> {
@@ -12,3 +12,6 @@ let updateArray = numbers.map((number)=> {
     }
 })
 console.log(updateArray);
+*/
+
+// let updateArray = numbers.map(()=>{});
