ae610de6c7b8fa772b38c04b6ba8f88949759717
atharatta2@gmail.com <atharatta2@gmail.com>
2023-09-03 17:20:04 +0500

    class

--- css.ts
+++ css.ts
@@ -1,3 +1,5 @@
+// 27/08/2023
+
 // map => return same lenght array
 // filter => return same lenght or less
 // forEach => return nothing
@@ -64,4 +66,116 @@ let arrayOfNames : string[] = ["AZHAR", "ATHAR", "AFTAB", "SAHIL", "MEHTAB"];
 */
 // end
 
-  
+// 3/9/2023
+
+// Leetcode
+
+// write a ts program that reshape the input array [2, 4, 10, 20]
+// to multiply by 2 and print the new list.
+/*
+var list:number[] = [2, 4, 10, 20];
+var newList:number[] = []
+
+for (let index = 0; index < list.length; index++ ){
+    const element = list[index];
+    newList.push(element * 2)
+}
+  */
+
+//  String   Number    Boolean   undefined   null    array
+
+// names, rollno,  session
+/*
+let names: string[] = ["Athar", "Azhar", "Aftab", "Akash"];
+let student1 = ["Athar" 121, "Morning"]
+let student2 = ["Azhar" 131, "Evening"]
+let student3 = ["Aftab" 134, "Morning"]
+*/
+
+// object
+/*
+let student1 = {
+    name: "Athar",
+    rollNo: 121,
+}
+console.log(student1.name)
+
+let student2 = {
+    rollNo: 131,
+    name: "Azhar",
+}
+console.log(student2.name)
+*/
+/*
+let student1 = {
+    name: "Athar",
+    rollNo: 121,
+    isActive: false,
+}
+console.log(student1.name)
+*/
+/*
+let student2 = {
+    rollNo: 131,
+    name: "Azhar",
+    isActive: true,
+    Adress: "House Node.12, D type, Fasialabad, Punjab, Pakistan"
+    city : ["Fasialabad", "Kohat", "Pindi"]
+}
+;
+console.log(student2.city.push("Lahore"))
+console.log(student2.city)
+*/
+/*
+let car;
+[
+{
+    model: "city",
+    brand: "Honda",
+    isUsed: true,
+    modelYear: 2003,
+    color: "Silver"
+},
+{
+    model: "city",
+    brand: "BMW",
+    isUsed: false,
+    modelYear: 2023,
+    color: "Black"
+},
+{
+    model: "cultus",
+    brand: "Suzki",
+    isUsed: true,
+    modelYear: 2009,
+    color: "Gray"
+},
+{
+    model: "corolla",
+    brand: "Toyota",
+    isUsed: false,
+    modelYear: 2013,
+    color: "White"
+}
+]
+console.log("car.brand")
+*/
+/*
+type CarType = {
+    model: string ,
+    brand:string ,
+    isUsed: boolean,
+    modelYear: number,
+    color: string,
+}
+*/
+/*
+let car = {
+    brand: "HONDA",
+    race: () => {
+        console.log("RACING")
+
+    }
+}
+car.race()
+*/
